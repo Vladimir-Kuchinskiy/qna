c4e28c097f493757748f4351fa0f9512ca430be8
Vladimir-Kuchinskiy <kuchinskiy.wladimir@gmail.com>
2018-07-12 22:58:20 +0300

    tests for answers#new passed

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,15 @@
 class AnswersController < ApplicationController
+
+  before_action :set_question
+
+  def new
+    @answer = @question.answers.new
+  end
+
+  private
+
+  def set_question
+    @question = Question.find(params[:question_id])
+  end
+
 end
