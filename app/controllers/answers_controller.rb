dd90ae30be58c01369655e068218a70cd31755a7
Vladimir-Kuchinskiy <kuchinskiy.wladimir@gmail.com>
2018-07-12 23:26:16 +0300

    answer#create tests passed

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -6,10 +6,23 @@ class AnswersController < ApplicationController
     @answer = @question.answers.new
   end
 
+  def create
+    @answer = @question.answers.new(answer_params)
+    if @answer.save
+      redirect_to @question, notice: 'Answer was successfully created'
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_question
     @question = Question.find(params[:question_id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body)
   end
 
 end
