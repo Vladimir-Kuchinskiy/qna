bce97ddadd99b2fb4cfd4f4bd9e425b99f394836
Vladimir-Kuchinskiy <kuchinskiy.wladimir@gmail.com>
2018-07-12 14:10:28 +0300

    answers#new test created

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :questions do
-    resources :answers, only: %i[new create]
+    resources :answers, shallow: true
   end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  let(:question) { create(:question) }
+  let(:answer) { create(:answer) }
 
+  describe 'GET #new' do
+    before { get :new, params: { question_id: question } }
+
+    it 'sets a question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'assigns a new Answer to a @question' do
+      expect(assigns(:answer)).to eq answer
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
 end
