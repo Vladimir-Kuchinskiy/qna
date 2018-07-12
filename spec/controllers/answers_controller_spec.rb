624c22561de3aa91bc49863e12cd93dda13725fb
Vladimir-Kuchinskiy <kuchinskiy.wladimir@gmail.com>
2018-07-12 22:58:20 +0300

    tests for answers#new passed

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -11,8 +11,8 @@ RSpec.describe AnswersController, type: :controller do
       expect(assigns(:question)).to eq question
     end
 
-    it 'assigns a new Answer to a @question' do
-      expect(assigns(:answer)).to eq answer
+    it 'assigns a new Answer to an @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
     end
 
     it 'renders new view' do
