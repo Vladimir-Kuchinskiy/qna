1f22cb053a284d175114f839d3b07921be3ca3e1
Vladimir-Kuchinskiy <kuchinskiy.wladimir@gmail.com>
2018-07-12 11:33:00 +0300

    test for questions#create

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,7 +13,20 @@ class QuestionsController < ApplicationController
 
   def edit; end
 
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to @question, notice: 'Question was successfully created'
+    else
+      render :new
+    end
+  end
+
   private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
 
   def set_question
     @question = Question.find(params[:id])
