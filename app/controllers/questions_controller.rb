54b5e094f511238b0f0ff68e1dd576ab366d6aba
Vladimir-Kuchinskiy <kuchinskiy.wladimir@gmail.com>
2018-07-13 18:18:51 +0300

    refactored questions_controller specs for authenticated users

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class QuestionsController < ApplicationController
-  before_action :authenticate_user!, only: %i[new create edit update]
-  before_action :set_question, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: %i[index show]
+  before_action :set_question,       only: %i[show edit update destroy]
 
   def index
     @questions = Question.all
