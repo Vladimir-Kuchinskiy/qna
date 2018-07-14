216ff8d2904ed676853cff9c7db6477b06bf8138
Vladimir-Kuchinskiy <kuchinskiy.wladimir@gmail.com>
2018-07-14 17:25:42 +0300

    refactored acceptanse sign in tests

--- spec/acceptance/sign_in_spec.rb
+++ spec/acceptance/sign_in_spec.rb
@@ -6,13 +6,11 @@ feature 'User sign in', %q{
   I want to be able to sign in
 } do
 
-  scenario 'Registered user is tries to sign in' do
-    User.create!(email: 'user@test.com', password: '12345678')
+  given(:user) { create(:user) }
 
-    visit new_user_session_path
-    fill_in 'Email',    with: 'user@test.com'
-    fill_in 'Password', with: '12345678'
-    click_on 'Sign in'
+  scenario 'Registered user is tries to sign in' do
+
+    sign_in(user)
 
     expect(page).to have_content 'Signed in successfully.'
     expect(current_path).to eq root_path
