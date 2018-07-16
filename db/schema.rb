5ccf324ab6d01cab06bce5a16a84d17231e1066b
Vladimir-Kuchinskiy <kuchinskiy.wladimir@gmail.com>
2018-07-16 14:05:14 +0300

    models tests added

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_13_134240) do
+ActiveRecord::Schema.define(version: 2018_07_16_105739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2018_07_13_134240) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
