58b899ad51bde601d84a25e205214c4c44d6d81b
Vladimir-Kuchinskiy <kuchinskiy.wladimir@gmail.com>
2018-07-16 12:42:16 +0300

    rubocop autocorrection

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,6 @@
-class Answer < ApplicationRecord
+# frozen_string_literal: true
 
+class Answer < ApplicationRecord
   validates :body, presence: true
   belongs_to :question
-
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,8 +1,8 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-
   it { should validate_presence_of :body }
   it { should belong_to(:question) }
-
 end
