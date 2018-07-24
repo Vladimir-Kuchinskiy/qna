eba97a778ce85170af4a2690d7d0969687a6a7a1
Vladimir-Kuchinskiy <kuchinskiy.wladimir@gmail.com>
2018-07-24 12:22:03 +0300

    redesign questions index

--- spec/acceptance/show_questions_list_spec.rb
+++ spec/acceptance/show_questions_list_spec.rb
@@ -13,6 +13,6 @@ feature 'Show questions list', '
   scenario 'User tries to visit questions page with all the questions' do
     questions
     visit questions_path
-    expect(page).to have_css('#questions_table tbody tr', count: 5)
+    expect(page).to have_css('.jumbotron', count: 5)
   end
 end
