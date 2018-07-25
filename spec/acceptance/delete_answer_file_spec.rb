b927dad84f4ab278549dc0a26b510718e9b50e09
Vladimir-Kuchinskiy <kuchinskiy.wladimir@gmail.com>
2018-07-26 02:33:39 +0300

    delete answer files acceptance tests added and passed

---
+++ spec/acceptance/delete_answer_file_spec.rb
@@ -0,0 +1,36 @@
+require_relative 'acceptance_helper'
+
+feature 'Delete file from question', '
+  In order to remove files from question
+  As an author of a question
+  I would like to be able to delete files
+' do
+
+  given(:user)      { create(:user) }
+  given!(:question) { create(:question, user: create(:user)) }
+  given!(:answer)   { create(:answer, user: user, attachments: [create(:attachment)], question: question) }
+
+  describe 'Author' do
+    before do
+      sign_in(user)
+      visit question_path(question)
+    end
+    scenario 'sees link for file delete' do
+      within('.answers') { expect(page).to have_link 'Delete file' }
+    end
+    scenario 'tries to delete file from question', js: true do
+      within('.answers') do
+        click_on 'Delete file'
+        expect(page).to_not have_link 'rails_helper.rb', href: '/uploads/attachment/file/1/rails_helper.rb'
+      end
+      expect(page).to have_content 'Your file was successfully deleted'
+    end
+  end
+
+  scenario 'Non-author tries to delete file from question' do
+    sign_in(user)
+    answer.update(user_id: create(:user).id)
+    visit question_path(question)
+    within('.answers') { expect(page).to_not have_link 'Delete file' }
+  end
+end
