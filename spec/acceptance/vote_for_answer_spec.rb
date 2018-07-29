250b85491cd5ad0822872c6e715db70ba278f6aa
Vladimir-Kuchinskiy <kuchinskiy.wladimir@gmail.com>
2018-07-29 14:08:56 +0300

    acceptance test for give vote for an answer added

--- spec/acceptance/vote_for_answer_spec.rb
+++ spec/acceptance/vote_for_answer_spec.rb
@@ -1,41 +1,42 @@
 require_relative 'acceptance_helper'
 
-feature 'Give a vote for a question', '
-  In order to pick up the most useful/useless question
+feature 'Give a vote for an answer', '
+  In order to pick up the most useful/useless answer
   As a user
   I would like to be able to give my vote
 ' do
 
   given(:user) { create(:user) }
   given(:author) { create(:user) }
-  given!(:question) { create(:question, user: author) }
+  given!(:question) { create(:question, user: user) }
+  given!(:answer) { create(:answer, question: question, user: author) }
 
   describe 'User' do
     before do
       sign_in(user)
-      visit questions_path
+      visit question_path(question)
     end
 
-    scenario 'tries to give a positive vote for a question', js: true do
-      votes_count = question.votes_count
-      within '.questions' do
+    scenario 'tries to give a positive vote for an answer', js: true do
+      votes_count = answer.votes_count
+      within '.answers' do
         click_on 'Like'
-        within('.votes') { expect(page).to have_content((votes_count + 1).to_s) }
+        within('.answer-votes') { expect(page).to have_content((votes_count + 1).to_s) }
       end
       expect(page).to have_content 'Your vote was successfully added'
     end
 
-    scenario 'tries to give a negative vote for a question', js: true do
+    scenario 'tries to give a negative vote for an answer', js: true do
       votes_count = question.votes_count
-      within '.questions' do
+      within '.answers' do
         click_on 'Dislike'
-        within('.votes') { expect(page).to have_content((votes_count - 1).to_s) }
+        within('.answer-votes') { expect(page).to have_content((votes_count - 1).to_s) }
       end
       expect(page).to have_content 'Your vote was successfully added'
     end
 
     scenario 'tries to give more than 1 vote for a question', js: true do
-      within '.questions' do
+      within '.answers' do
         click_on 'Like'
         expect(page).to_not have_link 'Like'
         expect(page).to_not have_link 'Dislike'
@@ -44,10 +45,10 @@ feature 'Give a vote for a question', '
   end
 
   describe 'Author' do
-    scenario 'tries to give any vote for a question' do
+    scenario 'tries to give any vote for an aswer' do
       sign_in(author)
-      visit questions_path
-      within '.questions' do
+      visit question_path(question)
+      within '.answer' do
         expect(page).to_not have_link 'Like'
         expect(page).to_not have_link 'Dislike'
       end
